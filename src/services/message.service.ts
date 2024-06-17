2d6a6b21bf483f5610fcb747b1d9b4837750713d
John Olatubosun <toluolatubosun@gmail.com>
2024-06-18 00:31:17 +0100

    feat: Add getAllByAppointmentId method to MessageService

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -50,6 +50,56 @@ class MessageService {
         return message;
     }
 
+    async getAllByAppointmentId({ query, params, $currentUser }: Partial<Request>) {
+        const { error, value: data } = Joi.object({
+            query: Joi.object({
+                page: Joi.number().default(1),
+                limit: Joi.number().default(10),
+            }).required(),
+            params: Joi.object({
+                appointment_id: Joi.string().required(),
+            }).required(),
+            $currentUser: Joi.object({
+                _id: Joi.required(),
+                role: Joi.string().valid("doctor", "patient").required(),
+            }).required(),
+        })
+            .options({ stripUnknown: true })
+            .validate({ query, params, $currentUser });
+        if (error) throw new CustomError(error.message, 400);
+
+        const appointment = await AppointmentModel.findOne({ _id: data.params.appointment_id });
+        if (!appointment) throw new CustomError("appointment not found", 404);
+
+        if (String(appointment.patient_ref) !== String(data.$currentUser._id) && String(appointment.doctor_ref) !== String(data.$currentUser._id)) {
+            throw new CustomError("unauthorized access, you are not part of this appointment", 403);
+        }
+
+        const options = {
+            page: data.query.page,
+            limit: data.query.limit,
+            sort: { created_at: -1 },
+            populate: { path: "sender_ref doctor_profile_ref patient_profile_ref" },
+            customLabels: {
+                prevPage: "prev_page",
+                nextPage: "next_page",
+                totalDocs: "total_docs",
+                totalPages: "total_pages",
+                hasPrevPage: "has_prev_page",
+                hasNextPage: "has_next_page",
+                pagingCounter: "paging_counter",
+            },
+        };
+
+        const filter: Record<string, any> = {
+            appointment_ref: appointment._id,
+        };
+
+        const messages = await MessageModel.paginate(filter, options);
+
+        return messages;
+    }
+
     async pusherAuthenticateUser({ body, $currentUser }: Partial<Request>) {
         const { error, value: data } = Joi.object({
             body: Joi.object({
