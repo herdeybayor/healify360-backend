c6ad5483a24f97204eb97aa0125261640b72beb1
John Olatubosun <toluolatubosun@gmail.com>
2024-06-17 22:23:26 +0100

    current user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { CONFIGS } from "@/configs";
+import auth from "@/middleware/auth.middleware";
 import UserCtrl from "@/controllers/user.controller";
 
 const router: Router = Router();
 
-router.post("/session", UserCtrl.getSession);
+router.post("/session", auth(CONFIGS.APP_ROLES.USER), UserCtrl.getSession);
 
 export default router;
