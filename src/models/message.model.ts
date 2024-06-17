4f24f97e53578b125a0fb0fcb559375f24ac2c2c
John Olatubosun <toluolatubosun@gmail.com>
2024-06-18 00:28:23 +0100

    feat: Add doctor and patient profile references to message model

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -3,11 +3,16 @@ import paginate from "mongoose-paginate-v2";
 
 import { IUser } from "@/models/user.model";
 import { IAppointment } from "@/models/appointment.model";
+import { IDoctorProfile } from "@/models/doctor-profile.model";
+import { IPatientProfile } from "@/models/patient-profile.model";
 
 export interface IMessage extends mongoose.Document {
     message: string;
+    sent_by: "doctor" | "patient";
     sender_ref: IUser | mongoose.Types.ObjectId;
     appointment_ref: IAppointment | mongoose.Types.ObjectId;
+    doctor_profile_ref: IDoctorProfile | mongoose.Types.ObjectId;
+    patient_profile_ref: IPatientProfile | mongoose.Types.ObjectId;
     created_at: Date;
     updated_at: Date;
 }
@@ -16,6 +21,11 @@ const messageSchema = new mongoose.Schema<IMessage>(
     {
         message: {
             type: String,
+            required: true,
+        },
+        sent_by: {
+            type: String,
+            enum: ["doctor", "patient"],
             required: true,
         },
         sender_ref: {
@@ -27,6 +37,14 @@ const messageSchema = new mongoose.Schema<IMessage>(
             type: mongoose.Schema.Types.ObjectId,
             ref: "appointments",
             required: true,
+        },
+        doctor_profile_ref: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "doctor_profiles",
+        },
+        patient_profile_ref: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "patient_profiles",
         },
     },
     {
